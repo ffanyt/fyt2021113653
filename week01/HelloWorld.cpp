9399643477b59f5bbea106aa3bb24a0ec36e9d5d
Fan <1378451643@qq.com>
2022-07-16 13:37:24 +0800

    打印Hello World

---
+++ week01/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+/**
+ * @brief 这是一个hello world打印函数
+ * @return int
+ * @author Fan (1378451643@qq.com)
+ */
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    getchar();
+    return 0;
+}
